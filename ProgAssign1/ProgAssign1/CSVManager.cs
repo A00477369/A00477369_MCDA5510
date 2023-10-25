d28e248e340c2f02650fb71298daf6268ce7d99f
A00477369 <neeyati.mehta@smu.ca>
2023-10-25 18:13:43 -0300

    Added Code for Assignment1 to the Directory

---
+++ ProgAssign1/ProgAssign1/CSVManager.cs
@@ -0,0 +1,130 @@
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using System.Globalization;
+using Microsoft.VisualBasic.FileIO;
+using System.Text.RegularExpressions;
+
+namespace ProgAssign1
+{
+	public class CSVManager
+	{
+        
+
+        public static void parse(String fileName)
+        {
+            Console.WriteLine("Parsing file for valid data . . .");
+            try
+            {
+                using (TextFieldParser parser = new TextFieldParser(fileName))
+                {
+                    //Extracting Date
+                    string regexPattern = @"\b(\d{4}/\d{1,2}/\d{1,2})\b";                  
+                    Match match = Regex.Match(fileName, regexPattern);
+                    string extractedDate = match.Groups[1].Value;
+
+
+                    parser.TextFieldType = FieldType.Delimited;
+                    parser.SetDelimiters(",");
+                    //Console.WriteLine(string.Join(" ", parser.ReadFields()));
+                    parser.ReadFields(); //skipping headers
+
+                    int c = 0;
+
+                    while (!parser.EndOfData)
+                    {
+                        
+                        if (c == 0)
+                        {
+                            c = 1;
+                            continue;
+                        }
+
+                      
+                        //Process row
+                        List < String > fields = new List<String>();
+                        fields.Add(extractedDate);
+                        fields.AddRange(parser.ReadFields());
+                
+                        if (fields.Contains(""))
+                        {
+                            Console.WriteLine("INCOMPLETE ROW---Skipped");
+                            DirWalker.skippedRows = DirWalker.skippedRows + 1;
+                        }
+                        else
+                        {
+                            DirWalker.validRows = DirWalker.validRows + 1;
+                            WriteToCsvFile(DirWalker.outputFile, fields);
+                            
+                        }
+
+
+                    }
+                }
+
+            }
+            catch (IOException ioe)
+            {
+                Console.WriteLine(ioe.StackTrace);
+            }
+            return;
+
+        }
+        public static void WriteToCsvFile(string filePath, List<String> fields)
+        {
+            try
+            {
+
+                FileStream fs = new FileStream(filePath, FileMode.Append);
+                using (var writer = new StreamWriter(fs))
+                using (var csv = new CsvWriter(writer, new CsvConfiguration(CultureInfo.InvariantCulture)))
+                {
+                    string row = "";
+                    foreach (string field in fields)
+                    {
+                        row = row + field + ", ";
+                        csv.WriteField(field);
+                    }
+                    Console.WriteLine(row);
+                    csv.NextRecord();
+                }
+
+                fs.Close();
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("The file or directory cannot be found.");
+            }
+            catch (DirectoryNotFoundException)
+            {
+                Console.WriteLine("The file or directory cannot be found.");
+            }
+            catch (DriveNotFoundException)
+            {
+                Console.WriteLine("The drive specified in 'path' is invalid.");
+            }
+            catch (PathTooLongException)
+            {
+                Console.WriteLine("'path' exceeds the maxium supported path length.");
+            }
+            catch (UnauthorizedAccessException)
+            {
+                Console.WriteLine("You do not have permission to create this file.");
+            }
+            catch (IOException e) when ((e.HResult & 0x0000FFFF) == 32)
+            {
+                Console.WriteLine("There is a sharing violation.");
+            }
+            catch (IOException e) when ((e.HResult & 0x0000FFFF) == 80)
+            {
+                Console.WriteLine("The file already exists.");
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine($"An exception occurred:\nError code: " +
+                                  $"{e.HResult & 0x0000FFFF}\nMessage: {e.Message}");
+            }
+
+        }
+    }
+}
+
---
+++ ProgAssign1/ProgAssign1/DirWalker.cs
@@ -0,0 +1,90 @@
+﻿using System.Diagnostics;
+
+namespace ProgAssign1
+{
+
+
+    public class DirWalker
+    {
+        //Setting up initial variables
+        public static string rootDir = "/Users/mehta/Desktop/smu/Sem1/Software Development/Repos/A00477369_MCDA5510/ProgAssign1/ProgAssign1/";
+        public static string logFile = rootDir + "logs/logs.txt";
+        public static string inputDir = rootDir + "Sample Data";
+        public static string outputFile = rootDir + "Output/output.csv";
+        public static long validRows = 0;
+        public static long skippedRows = 0;
+
+        public void walk(String path)
+        {
+
+            string[] list = Directory.GetDirectories(path);
+
+
+            if (list == null) return;
+
+            foreach (string dirpath in list)
+            {
+                if (Directory.Exists(dirpath))
+                {
+                    walk(dirpath);
+                    Console.WriteLine("Dir:" + dirpath);
+                }
+            }
+            string[] fileList = Directory.GetFiles(path);
+            foreach (string filepath in fileList)
+            {
+
+                Console.WriteLine("File:" + filepath);
+                CSVManager.parse(filepath);
+            }
+
+            return;
+        }
+
+        public static void Main(String[] args)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+
+            //Start the stopwatch
+            stopwatch.Start();
+        
+            //Setting up logs
+            FileStream fs = new FileStream(logFile, FileMode.Append);
+            var log = new StreamWriter(fs);
+            Console.SetOut(log);
+
+            Console.WriteLine("-----------Program Started----------");
+
+            //Generating headers
+            Console.WriteLine("Generating Headers. . . ");
+            List<String> headers = new List<string>();
+            headers.Add("Date");
+            headers.Add("First Name");
+            headers.Add("Last Name");
+            headers.Add("Street Number");
+            headers.Add("Street");
+            headers.Add("City");
+            headers.Add("Province");
+            headers.Add("Country");
+            headers.Add("Postal Code");
+            headers.Add("Phone Number");
+            headers.Add("Email Address");
+            CSVManager.WriteToCsvFile(outputFile, headers);
+            DirWalker fw = new DirWalker();
+            fw.walk(inputDir);
+
+            //Stop the stopwatch
+            stopwatch.Stop();
+
+            Console.WriteLine("-----------Program Stopped----------");
+
+            long elapsedMilliseconds = stopwatch.ElapsedMilliseconds;
+            double elapsedSeconds = (double)elapsedMilliseconds / 10000;
+            Console.WriteLine($"Execution time: {elapsedSeconds} seconds");
+            Console.WriteLine($"Total valid rows: {validRows}");
+            Console.WriteLine($"Total skipped rows: {skippedRows}");
+            log.Flush();
+        }
+
+    }
+}
